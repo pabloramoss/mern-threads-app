a995e38e2ee482ad6fad80821f4751807366a4b7
pabloramoss <pablo-362@hotmail.com>
2023-09-08 08:27:29 +0200

    feat: add lesftisidebar

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,5 +1,46 @@
+'use client';
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname, useRouter } from 'next/navigation';
+import { SignedIn, SignedOut } from '@clerk/nextjs';
+
+import { sidebarLinks } from '@/constants';
+
 const LeftSidebar: React.FC = () => {
-  return <h1>LeftSidebar</h1>;
+  const router = useRouter();
+  const pathname = usePathname();
+
+  return (
+    <section className="custom-scrollbar leftsidebar">
+      <div className="flex w-full flex-1 flex-col gap-6 px-6">
+        {sidebarLinks.map((link) => {
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route;
+
+          return (
+            <Link
+              key={link.label}
+              className={`leftsidebar_link ${isActive && 'bg-primary-500'}`}
+              href={link.route}
+            >
+              <Image alt={link.label} height={24} src={link.imgURL} width={24} />
+              <p className="text-light-1 max-lg:hidden">{link.label}</p>
+            </Link>
+          );
+        })}
+      </div>
+      <div className="mt-10 px-6">
+        <SignedIn>
+          <SignedOut>
+            <div className="flex cursor-pointer">
+              <Image alt="logout" height={24} src="/assets/logout.svg" width={24} />
+              <button>asdf</button>
+            </div>
+          </SignedOut>
+        </SignedIn>
+      </div>
+    </section>
+  );
 };
 
 export default LeftSidebar;
--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,5 +1,30 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { OrganizationSwitcher, SignedIn, SignedOut } from '@clerk/nextjs';
+
 const Topbar: React.FC = () => {
-  return <h1>Topbar</h1>;
+  return (
+    <nav className="topbar">
+      <Link className="flex items-center gap-4" href="/">
+        <Image alt="logo" height={28} src="/assets/logo.svg" width={28} />
+        <p className="text-heading-3-bold text-light-1 max-xs:hidden">Threads</p>
+      </Link>
+      <div className="flex items-center gap-1">
+        <div className="block md:hidden">
+          <SignedIn>
+            <SignedOut>
+              <div className="flex cursor-pointer">
+                <Image alt="logout" height={24} src="/assets/logout.svg" width={24} />
+              </div>
+            </SignedOut>
+          </SignedIn>
+        </div>
+        <OrganizationSwitcher
+          appearance={{ elements: { organizationSwitcherTrigger: 'py-2 px-4' } }}
+        />
+      </div>
+    </nav>
+  );
 };
 
 export default Topbar;
